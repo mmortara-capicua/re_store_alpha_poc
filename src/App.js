a09beb49c024f100be12147be16683b8125b51d1
AxiomaAbsurdo <m.mortara01@gmail.com>
2020-07-08 12:29:20 -0300

    fix:grid and containers add: grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="layout-container">
-      <div className="header">
-        header
-      </div>
+      <div className="header">header</div>
       <div className="main">
-        main
+        <div className="grid-container">
+          <div className="ChatForm">ChatForm</div>
+          <div className="ChatTimeLine">ChatTimeLine</div>
+          <div className="VideoPlayer">VideoPlayer</div>
+        </div>
       </div>
-      <div className="footer">
-        footer
-      </div>
+      <div className="footer">footer</div>
     </div>
   );
 }
